e7f70e5200e61d5331518d1933059b63225ebbcc
akhilgoplani <30722928+akhilgoplani@users.noreply.github.com>
2017-08-16 21:16:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 // Move the image
-var img = document.getElementById('img')
+var img = document.getElementById('madi')
 img.oneclick = function () {
     img.style.marginleft = '100px';
 };
