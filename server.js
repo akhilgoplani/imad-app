a9e0467a2d3fec7e0fc8dbc8e66268a09f454bdc
akhilgoplani <30722928+akhilgoplani@users.noreply.github.com>
2017-08-23 22:00:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.get('/article-three', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
+app.get('/SPECIAL', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'SPECIAL.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
